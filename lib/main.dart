dadafa162fa0e98fc28d0b7abf8113395c2ef84c
Marko Hrastovec <marko.hrastovec@gmail.com>
2021-11-01 21:22:52 +0100

    Playing with flutter widgets and learning.

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,10 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'generated/l10n.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(TarockApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class TarockApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,38 +21,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
-      home: MyHomePage(currentLocale: 'en'),
+      home: TarockHomePage(currentLocale: 'en'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.currentLocale}) : super(key: key);
+class TarockHomePage extends StatefulWidget {
+  TarockHomePage({Key? key, required this.currentLocale}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
   String currentLocale = 'en';
+  int numOfPlayers = 3;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _TarockHomePageState createState() => _TarockHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _TarockHomePageState extends State<TarockHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -64,58 +51,94 @@ class _MyHomePageState extends State<MyHomePage> {
       DropdownMenuItem<String> (value: "en", child: Text ("English")),
       DropdownMenuItem<String> (value: "si", child: Text ("Slovensko"))
     ];
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(S.of (context).appTitle),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DropdownButton<String> (
-              value: widget.currentLocale,
-              onChanged: (String? newValue) {
-                setState(() {
-                  widget.currentLocale = newValue!;
-                  S.load (Locale (widget.currentLocale, ''));
-                });
-              },
-              items: languages,
-            ),
-            Text (S.of (context).appMessage),
-            Text ('$_counter', style: Theme.of(context).textTheme.headline4),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Container (
+                child: Text (S.of (context).currentLocaleLabel),
+                margin: const EdgeInsets.all (5.0),
+              ),
+              Container (
+                child: DropdownButton<String> (
+                  value: widget.currentLocale,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      widget.currentLocale = newValue!;
+                      S.load (Locale (widget.currentLocale, ''));
+                    });
+                  },
+                  items: languages,
+                ),
+                margin: const EdgeInsets.all (5.0),
+              ),
+            ]
+          ),
+          Row (
+            children: <Widget>[
+              Container (
+                child: Text (S.of (context).appMessage),
+                margin: const EdgeInsets.all (5.0),
+              ),
+              Container (
+                child: Text ('$_counter'),
+                margin: const EdgeInsets.all(5.0),
+              ),
+            ]
+          ),
+          Row(
+            children: <Widget>[
+              Container(
+                child: Text (S.of(context).numOfPlayersLabel),
+                margin: const EdgeInsets.all(5.0),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    ChoiceChip(
+                      label: Text('3'),
+                      selected: widget.numOfPlayers == 3,
+                      onSelected: (bool selected) {
+                        setState(() {
+                          if (selected) widget.numOfPlayers = 3;
+                        });
+                      },
+                      avatar: CircleAvatar(
+                        backgroundColor: widget.numOfPlayers != 3 ? Colors.transparent : null,
+                        child: widget.numOfPlayers == 3 ? Icon (Icons.check) : null,
+                      ),
+                    ),
+                    ChoiceChip(
+                      label: Text('4'),
+                      selected: widget.numOfPlayers == 4,
+                      onSelected: (bool selected) {
+                        setState(() {
+                          if (selected) widget.numOfPlayers = 4;
+                        });
+                      },
+                      avatar: CircleAvatar(
+                        backgroundColor: widget.numOfPlayers != 4 ? Colors.transparent : null,
+                        child: widget.numOfPlayers == 4 ? Icon (Icons.check) : null,
+                      ),
+                    ),
+                  ],
+                ),
+                margin: const EdgeInsets.all(5.0),
+              )
+            ]
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: S.of (context).incrementLabel,
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
