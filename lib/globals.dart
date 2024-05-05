0f76c4cc037b200a35dd74fa729979d1fabf7195
Marko Hrastovec <marko.hrastovec@gmail.com>
2024-05-05 19:06:18 +0200

    Translating form C# and figuring out how to organize classes.

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,2 +1,5 @@
-String cardBackFace = 'cards/backs/slo/';
-String cardFace = 'cards/faces/slo/';
+List<String> cardBackFace = <String>['slo', 'slo2' ];
+List<String> cardFace = <String>[ 'slo', 'slo2' ];
+
+String currentBackFace = 'slo';
+String currentFace = 'slo2';
--- lib/ui/tarok_game.dart
+++ lib/ui/tarok_game.dart
@@ -13,66 +13,64 @@ class TarokGame extends FlameGame {
   TarokGame ()
   : super ()
   {
-    List<String> face = <String>[
-      'slo',
-      'slo2',
-    ];
-    for (int i = 0; i < face.length; i++) {
-      imageFile.add ('cards/backs/${face[i]}/backside.png');
-      imageFile.add ('cards/faces/${face[i]}/clubs_7.png');
-      imageFile.add ('cards/faces/${face[i]}/clubs_8.png');
-      imageFile.add ('cards/faces/${face[i]}/clubs_9.png');
-      imageFile.add ('cards/faces/${face[i]}/clubs_10.png');
-      imageFile.add ('cards/faces/${face[i]}/clubs_jack.png');
-      imageFile.add ('cards/faces/${face[i]}/clubs_knight.png');
-      imageFile.add ('cards/faces/${face[i]}/clubs_queen.png');
-      imageFile.add ('cards/faces/${face[i]}/clubs_king.png');
-      imageFile.add ('cards/faces/${face[i]}/diamonds_1.png');
-      imageFile.add ('cards/faces/${face[i]}/diamonds_2.png');
-      imageFile.add ('cards/faces/${face[i]}/diamonds_3.png');
-      imageFile.add ('cards/faces/${face[i]}/diamonds_4.png');
-      imageFile.add ('cards/faces/${face[i]}/diamonds_jack.png');
-      imageFile.add ('cards/faces/${face[i]}/diamonds_knight.png');
-      imageFile.add ('cards/faces/${face[i]}/diamonds_queen.png');
-      imageFile.add ('cards/faces/${face[i]}/diamonds_king.png');
-      imageFile.add ('cards/faces/${face[i]}/hearts_1.png');
-      imageFile.add ('cards/faces/${face[i]}/hearts_2.png');
-      imageFile.add ('cards/faces/${face[i]}/hearts_3.png');
-      imageFile.add ('cards/faces/${face[i]}/hearts_4.png');
-      imageFile.add ('cards/faces/${face[i]}/hearts_knight.png');
-      imageFile.add ('cards/faces/${face[i]}/hearts_queen.png');
-      imageFile.add ('cards/faces/${face[i]}/hearts_jack.png');
-      imageFile.add ('cards/faces/${face[i]}/hearts_king.png');
-      imageFile.add ('cards/faces/${face[i]}/spades_7.png');
-      imageFile.add ('cards/faces/${face[i]}/spades_8.png');
-      imageFile.add ('cards/faces/${face[i]}/spades_9.png');
-      imageFile.add ('cards/faces/${face[i]}/spades_10.png');
-      imageFile.add ('cards/faces/${face[i]}/spades_jack.png');
-      imageFile.add ('cards/faces/${face[i]}/spades_knight.png');
-      imageFile.add ('cards/faces/${face[i]}/spades_queen.png');
-      imageFile.add ('cards/faces/${face[i]}/spades_king.png');
-      imageFile.add ('cards/faces/${face[i]}/I.png');
-      imageFile.add ('cards/faces/${face[i]}/II.png');
-      imageFile.add ('cards/faces/${face[i]}/III.png');
-      imageFile.add ('cards/faces/${face[i]}/IIII.png');
-      imageFile.add ('cards/faces/${face[i]}/V.png');
-      imageFile.add ('cards/faces/${face[i]}/VI.png');
-      imageFile.add ('cards/faces/${face[i]}/VII.png');
-      imageFile.add ('cards/faces/${face[i]}/VIII.png');
-      imageFile.add ('cards/faces/${face[i]}/IX.png');
-      imageFile.add ('cards/faces/${face[i]}/X.png');
-      imageFile.add ('cards/faces/${face[i]}/XI.png');
-      imageFile.add ('cards/faces/${face[i]}/XII.png');
-      imageFile.add ('cards/faces/${face[i]}/XIII.png');
-      imageFile.add ('cards/faces/${face[i]}/XIV.png');
-      imageFile.add ('cards/faces/${face[i]}/XV.png');
-      imageFile.add ('cards/faces/${face[i]}/XVI.png');
-      imageFile.add ('cards/faces/${face[i]}/XVII.png');
-      imageFile.add ('cards/faces/${face[i]}/XVIII.png');
-      imageFile.add ('cards/faces/${face[i]}/XIX.png');
-      imageFile.add ('cards/faces/${face[i]}/XX.png');
-      imageFile.add ('cards/faces/${face[i]}/XXI.png');
-      imageFile.add ('cards/faces/${face[i]}/skis.png');
+    for (int i = 0; i < globals.cardBackFace.length; i++) {
+      imageFile.add('cards/backs/${globals.cardBackFace[i]}/backside.png');
+    }
+    for (int i = 0; i < globals.cardFace.length; i++) {
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/clubs_7.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/clubs_8.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/clubs_9.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/clubs_10.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/clubs_jack.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/clubs_knight.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/clubs_queen.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/clubs_king.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/diamonds_1.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/diamonds_2.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/diamonds_3.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/diamonds_4.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/diamonds_jack.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/diamonds_knight.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/diamonds_queen.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/diamonds_king.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/hearts_1.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/hearts_2.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/hearts_3.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/hearts_4.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/hearts_knight.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/hearts_queen.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/hearts_jack.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/hearts_king.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/spades_7.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/spades_8.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/spades_9.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/spades_10.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/spades_jack.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/spades_knight.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/spades_queen.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/spades_king.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/I.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/II.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/III.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/IIII.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/V.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/VI.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/VII.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/VIII.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/IX.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/X.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XI.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XII.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XIII.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XIV.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XV.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XVI.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XVII.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XVIII.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XIX.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XX.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/XXI.png');
+      imageFile.add ('cards/faces/${globals.cardFace[i]}/skis.png');
     }
   }
 
